f49049b9a9a76f35d4ea8cec04e5649c477e2bfa
Aryan191201 <93042322+Aryan191201@users.noreply.github.com>
2022-02-24 20:37:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Buttons from './Components/Buttons';
+
+function App(){
+  return (
+    <div className="app">
+      <Buttons />
+    </div>
+  )
+}
+
+export default App;
